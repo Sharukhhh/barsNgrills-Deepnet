061bd6af853248a46e14be9b7e49e969dcd4367c
Sharuk <msharuk.b10@gmail.com>
2024-05-04 18:44:47 +0530

    change in end points

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 
 export const Axios = axios.create({
     // baseURL :'http://localhost:3000/api/menu',
-    baseURL : 'https://barsngrills-deepnet-backend.onrender.com'
+    baseURL : 'https://backend-barsngrills-deepnet.onrender.com'
 })
 
